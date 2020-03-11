413f919cf676f7e6db91a9728bfaad7968cae41b
Эдуард <alexed34@yandex.ru>
2020-03-11 22:13:37 +0400

    13 шаг

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -2,14 +2,16 @@ import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 
+def page(number):
+    response = requests.get(f'http://tululu.org/l55/{number}/')
+    response.raise_for_status()
+    soup = BeautifulSoup(response.text, 'lxml')
+    text_cards = soup.find_all('table', class_='d_book')
+    for card in text_cards:
+        link = card.find('a')['href']
 
-response = requests.get('http://tululu.org/l55/')
-response.raise_for_status()
-soup = BeautifulSoup(response.text, 'lxml')
-text_cards = soup.find_all('table', class_='d_book')
-for card in text_cards:
-    link = card.find('a')['href']
+        link = urljoin('http://tululu.org/', link)
+        print(link)
 
-    link = urljoin('http://tululu.org/', link)
-    print(link)
-
+for number in range(1,11):
+    page(number)
